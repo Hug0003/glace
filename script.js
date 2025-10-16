642a3033068419e22d4d3df82f328f0215a48318
Hug0003 <134955227+Hug0003@users.noreply.github.com>
2025-10-16 14:18:17 +0200

    opti img

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const tast_btns = document.querySelectorAll(".choice__btn");
 const place_btn = document.querySelector(".place__btn");
 const glace = document.querySelector(".glace");
+const glacePicture = document.querySelector(".glace-picture");
+const glaceSource = glacePicture ? glacePicture.querySelector("source") : null;
 const glace2 = document.querySelector(".poster__glace");
+const posterPicture = document.querySelector(".poster-picture");
+const posterSource = posterPicture ? posterPicture.querySelector("source") : null;
 const btnLetter = document.querySelector(".search__button");
 const textGlace = document.querySelector(".text__glace");
 
@@ -13,8 +17,10 @@ tast_btns.forEach((btn) =>
       e.target.classList.add("win");
       const color = e.target.dataset.color;
       const link = e.target.dataset.src;
-      glace.src = `img/glace-${link}.png`;
-      glace2.src = `img/glace-${link}.png`;
+      if (glaceSource) glaceSource.srcset = `img/glace-${link}.webp`;
+      if (glace) glace.src = `img/glace-${link}.png`;
+      if (posterSource) posterSource.srcset = `img/glace-${link}.webp`;
+      if (glace2) glace2.src = `img/glace-${link}.png`;
 
       console.log(btnLetter, color);
       btnLetter.style.backgroundColor = getComputedStyle(
